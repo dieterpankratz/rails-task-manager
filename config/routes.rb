3695820ad0f2d8b54766f6e7079fed0cccd2450a
Dieter Pankratz <pankratz117@gmail.com>
2019-11-12 13:38:52 +0100

    added the backt-to-list link on each single task page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Generic route:
   # VERB PATH, to: 'CONTROLLER#ACTION'
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
