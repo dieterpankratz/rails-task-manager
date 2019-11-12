bad011672d5daaeff2c51ef8757f4726450fe988
Dieter Pankratz <pankratz117@gmail.com>
2019-11-12 14:41:36 +0100

    creating a new task is now possible

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   # Generic route:
   # VERB PATH, to: 'CONTROLLER#ACTION'
   get '/tasks', to: 'tasks#index', as: 'tasks'
+
+  # GET the task creation form = 1 request
+  # POST the parameters to create a new task = 1 request
+  get '/tasks/new', to: 'tasks#new', as: 'new'
+  post '/tasks', to: 'tasks#create'
+
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id/', to: 'tasks#update'
 end
